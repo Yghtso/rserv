53db7256dc3dd4fc8c6206b0286d9bac36ffa0eb
Soare Cristian <soare.cristian@outlook.com>
2024-08-14 06:51:53 -0700

    Add an interaction between main and listener thread, use of Arc to make possible to read from the listener socket in the main thread

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod rservs;
 fn main() {
+    let (start_list_thread_tx, start_list_thread_rx) = std::sync::mpsc::channel::<bool>();
+    let (sessions_tx, sessions_rx) = std::sync::mpsc::channel();  
+    
+    let master_listener_thread = rservs::master::Listener::new(30000, start_list_thread_rx, sessions_tx);
+    start_list_thread_tx.send(true).unwrap();
+    master_listener_thread.listener_thread.unwrap().join().unwrap();
 
-    let (tx, rx) = std::sync::mpsc::channel::<bool>();
-    let master_listener_thread = rservs::master::Listener::new(30000,rx);
-
-    //tx.send(true).unwrap();
-
-    master_listener_thread.listener_thread.join().unwrap();
 }
--- src/rservs/master.rs
+++ src/rservs/master.rs
@@ -1,28 +1,41 @@
 pub struct Listener {
-    pub listener_thread: std::thread::JoinHandle<()>,
-    socket: std::net::TcpListener,
+    pub listener_thread: Option<std::thread::JoinHandle<()>>,
+    pub socket: Option<std::sync::Arc<std::sync::Mutex<std::net::TcpListener>>>,
 }
 
-impl Listener{
+impl Listener {
 
-    pub fn new(port: u16, receiver: std::sync::mpsc::Receiver<bool>) -> Listener {
+    pub fn new(port: u16, start_receiver: std::sync::mpsc::Receiver<bool>, session_streams_transmitter: std::sync::mpsc::Sender<(std::net::TcpStream, std::net::SocketAddr)>) -> Listener {
+        
+        let self_host: String = format!("127.0.0.1:{}", port);  
+        let sock: std::sync::Arc<std::sync::Mutex<std::net::TcpListener>> = 
 
-        let self_host = format!("127.0.0.1:{}", port);  
-        let handle = std::thread::spawn(move || {
-            let msg_received = receiver.recv().unwrap();
-            print!("MAIN THREAD IN ESECUZIONE");
+            std::sync::Arc::new(
+
+                std::sync::Mutex::new(
+                
+                    std::net::TcpListener::bind(self_host.as_str())
+                    .expect("Errore nell inizializzazione del thread listener")));
+        
+        let sock_clone = std::sync::Arc::clone(&sock);
+
+
+        let handle: std::thread::JoinHandle<()> = std::thread::spawn(move || {
+            start_receiver.recv().unwrap();
+
+            let sock: std::sync::MutexGuard<std::net::TcpListener> = sock_clone.try_lock().unwrap();
+            match sock.accept() {
+                Ok((socket, addr)) => {
+                    let connection = (socket, addr);
+                    session_streams_transmitter.send(connection);
+                }
+                Err(e) => println!("couldn't get client: {e:?}"),
+            }
         });
 
         Listener {
-            listener_thread: handle,
-            socket: std::net::TcpListener::bind(self_host.as_str()).expect("Errore nell inizializzazione del thread listener"),
-        }
-    }
-
-    fn handle_income_connections(&self) {
-        match self.socket.accept() {
-            Ok((socket, addr)) => println!("new client: {addr:?}"),
-            Err(e) => println!("couldn't get client: {e:?}"),
+            listener_thread: Some(handle),
+            socket: Some(sock),
         }
     }
 }
