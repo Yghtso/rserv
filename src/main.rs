a2461f756726f1ce5c09d458b075e77fb828593a
Soare Cristian <soare.cristian@outlook.com>
2024-08-15 07:00:54 -0700

    Add some comments for clarity

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 mod rservs;
 fn main() {
+
+    let sessions: Vec<rservs::session::Client> = Vec::new();
+
+    // creating the channels for the Listener thread and the main thread
+
+    // channel to comunicate for the start of the listener thread
     let (start_list_thread_tx, start_list_thread_rx) = std::sync::mpsc::channel::<bool>();
+    // channel to comunicate the incoming connections
     let (sessions_tx, sessions_rx) = std::sync::mpsc::channel();  
-    
+
+
+    // istantiating the Listener thread
     let master_listener_thread = rservs::master::Listener::new(30000, start_list_thread_rx, sessions_tx);
+    // starting the thread
     start_list_thread_tx.send(true).unwrap();
+
+    
+
     master_listener_thread.listener_thread.unwrap().join().unwrap();
 
 }
--- src/rservs/master.rs
+++ src/rservs/master.rs
@@ -1,24 +1,25 @@
 pub struct Listener {
+    // thread that shares a socket Arc(Atomic reference count)
     pub listener_thread: Option<std::thread::JoinHandle<()>>,
+    // socket that listens for income connections
     pub socket: Option<std::sync::Arc<std::sync::Mutex<std::net::TcpListener>>>,
 }
 
 impl Listener {
 
+    // arguments : port;on wich the server star   -   receiver;that gets the signal to start the thread   -   transmitter; to send back the income connection
+    // returns : Listener; istance of type Listener
     pub fn new(port: u16, start_receiver: std::sync::mpsc::Receiver<bool>, session_streams_transmitter: std::sync::mpsc::Sender<(std::net::TcpStream, std::net::SocketAddr)>) -> Listener {
         
         let self_host: String = format!("127.0.0.1:{}", port);  
         let sock: std::sync::Arc<std::sync::Mutex<std::net::TcpListener>> = 
 
             std::sync::Arc::new(
-
                 std::sync::Mutex::new(
-                
                     std::net::TcpListener::bind(self_host.as_str())
-                    .expect("Errore nell inizializzazione del thread listener")));
+                    .expect("Error in the initialization of the listener thread")));
         
         let sock_clone = std::sync::Arc::clone(&sock);
-
 
         let handle: std::thread::JoinHandle<()> = std::thread::spawn(move || {
             start_receiver.recv().unwrap();
