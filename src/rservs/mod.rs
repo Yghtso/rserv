924caa5135075e2487ba2bc4d2b9b597cb601c20
Soare Cristian <soare.cristian@outlook.com>
2024-08-19 14:45:31 -0700

    Add a besic struct for the server to don' t have to initialize all in the main fn directly

--- src/rservs/mod.rs
+++ src/rservs/mod.rs
@@ -1,3 +1,17 @@
 pub mod master;
 pub mod session;
-//mod runnable;
+
+struct Server {
+    sessions: Vec<(session::Client, std::sync::mpsc::Sender<bool>)>,
+    listener: (master::Listener, std::sync::mpsc::Sender<bool>, std::sync::mpsc::Receiver<std::net::TcpStream>),
+}
+
+impl Server {
+
+    pub fn new() -> Self {
+        Server { 
+            sessions: Vec::new(),
+            listener: master::Listener::new(port, start_receiver, session_streams_transmitter)
+        }
+    }
+}
