d5fc66d1fdc8389f7744d96e3670ffb45a613780
Soare Cristian <soare.cristian@outlook.com>
2024-08-20 10:26:32 -0700

    Almost finished the Server struct

--- src/rserv/mod.rs
+++ src/rserv/mod.rs
@@ -1,11 +1,11 @@
 pub mod master;
 pub mod session;
 
-struct Server {
+pub struct Server {
     sessions: Vec<(session::Client, std::sync::mpsc::Sender<bool>)>,
     listener: Option<master::Listener>,
-    listener_control: Option<std::sync::mpsc::Sender<bool>>,
-    sessions_reciever: Option<std::sync::mpsc::Receiver<std::net::TcpStream>>,
+    listener_control_tx: Option<std::sync::mpsc::Sender<bool>>,
+    sessions_rx: Option<std::sync::mpsc::Receiver<std::net::TcpStream>>,
 }
 
 impl Server {
@@ -14,10 +14,31 @@ impl Server {
         Server {
             sessions: Vec::new(),
             listener: None,
-            listener_control: None,
-            sessions_reciever: None,
+            listener_control_tx: None,
+            sessions_rx: None,
         }
     }
 
-    
+    pub fn bind(mut self, address: std::net::SocketAddr) -> Self {
+        let (listener_control_tx, listener_control_rx) = std::sync::mpsc::channel::<bool>();
+        self.listener_control_tx = Some(listener_control_tx);
+
+        let (sessions_tx, sessions_rx) = std::sync::mpsc::channel::<std::net::TcpStream>();
+        self.sessions_rx = Some(sessions_rx);
+
+        self.listener = Some(master::Listener::new(address, listener_control_rx, sessions_tx));
+
+        self
+    }
+
+    pub fn run(& self) {
+        if self.listener.is_none() || self.listener_control_tx.is_none() || self.sessions_rx.is_none() {
+            println!("[ - ] Error, the server was trying to start but some necessary fields weren' t initialized");
+            return;
+        }
+
+        loop {
+            
+        }
+    }
 }
