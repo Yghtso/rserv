b4b6f8329d21885e1374e391fd0b95f7e536cbd5
Soare Cristian <soare.cristian@outlook.com>
2024-08-20 10:24:57 -0700

    Better understanding and clearer syntax instantiating the server

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,10 @@
-mod rservs;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+mod rserv;
 fn main() {
 
-    let mut sessions: Vec<(rservs::session::Client, std::sync::mpsc::Sender<bool>)> = Vec::new();
-
-    // creating the channels for the Listener thread and the main thread
-
-    // channel to comunicate for the start of the listener thread
-    let (start_list_thread_tx, start_list_thread_rx) = std::sync::mpsc::channel::<bool>();
-    // channel to comunicate the incoming connections
-    let (sessions_tx, sessions_rx) = std::sync::mpsc::channel();  
-
-    // istantiating the Listener thread
-    let master_listener_thread = rservs::master::Listener::new(30000, start_list_thread_rx, sessions_tx);
-    // starting the thread
-    start_list_thread_tx.send(true).unwrap();
-
-    loop {
-        let stream = sessions_rx.try_recv();
-        
-        match stream {
-            Ok(stream) => {
-                let (session_start_tx, session_start_rx) = std::sync::mpsc::channel::<bool>();
-                sessions.push((rservs::session::Client::new(stream, session_start_rx), session_start_tx));
-
-                let (client, tx) = sessions.last().unwrap();
-                tx.send(true);
-                println!("{}", sessions.len());
-            }
-            Err(e) => (),
-        }
-    }
+    let s: rserv::Server = rserv::Server::new()
+    .bind(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 30000));
+    
+    s.run();
 }
