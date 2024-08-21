31bfa48b522a9610421f5d0084f874bdc17d4bdd
Soare Cristian <soare.cristian@outlook.com>
2024-08-21 16:04:44 -0700

    Better out of the crate usage of the server

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-
+use std::net::{Ipv4Addr, SocketAddr};
 mod rserv;
 fn main() {
 
-    let s: rserv::Server = rserv::Server::new()
-    .bind(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 30000));
-    
-    s.run();
+    let s = rserv::Server::new()
+    .bind(SocketAddr::from((Ipv4Addr::new(127, 0, 0, 1), 8080)))
+    .run();
+
+    match s {
+        None => println!("[ - ] [MAIN] Error, the server couldn't be started"),
+        _ => (),
+    }
 }
