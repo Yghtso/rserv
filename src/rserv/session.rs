46e640c13ca2b3b9f0ac72b244b3c903cac3b9c8
Soare Cristian <soare.cristian@outlook.com>
2024-08-22 06:11:07 -0700

    Basic request/response session mechanism

--- src/rserv/session.rs
+++ src/rserv/session.rs
@@ -23,6 +23,8 @@ impl Client {
         let handle: std::thread::JoinHandle<()> = std::thread::Builder::new()
         .name(format!("[CLIENT #{}]", 1).into())
         .spawn(move || {
+
+            println!("[ + ] [CLIENT] new client spawned called {}", std::thread::current().name().unwrap_or("Unnamed"));
             loop {
                 // TODO : error handling 
                 let result = session_control_receiver.try_recv();
@@ -41,9 +43,8 @@ impl Client {
                     super::ThreadStatus::Blocked => if change_state {*status = super::ThreadStatus::Running},
 
                     super::ThreadStatus::Running => {
-                        let stream: std::sync::MutexGuard<std::net::TcpStream> = stream_clone.lock().unwrap();
                         
-                        // TODO : implement this
+                        Client::handle_request_response(std::sync::Arc::clone(&stream_clone));
 
                         if change_state {*status = super::ThreadStatus::Blocked};
                     }
@@ -51,11 +52,43 @@ impl Client {
                 }
             }
         }).expect("[ - ] [MAIN] Error a new [CLIENT] thread can' t be created");
-
         Client {
             session_thread: handle,
             stream,
             status,
+        }
+    }
+
+    fn handle_request_response(stream_ref: std::sync::Arc<std::sync::Mutex<std::net::TcpStream>>) {
+        let mut stream: std::sync::MutexGuard<std::net::TcpStream> = stream_ref.lock().unwrap();
+
+        let mut buffer = [0; 512];
+        match std::io::Read::read(&mut *stream, &mut buffer) {
+            Ok(n) => {
+                if n > 0 {
+                    let received_data = String::from_utf8_lossy(&buffer[..n]);
+
+                    let response = format!(
+                        "HTTP/1.1 200 OK\r\n\
+                        Content-Type: text/html; charset=UTF-8\r\n\
+                        Content-Length: {}\r\n\
+                        Connection: close\r\n\r\n\
+                        <html>\
+                            <head><title>Response</title></head>\
+                            <body>\
+                                <h1>Hello, World!</h1>\
+                                <p>This is a response from the server.</p>\
+                            </body>\
+                        </html>",
+                        "<html><head><title>Response</title></head><body><h1>Hello, World!</h1><p>This is a response from the server.</p></body></html>".len()
+                    );
+    
+                    std::io::Write::write(&mut (*stream), response.as_bytes()).unwrap();
+                }
+            }
+            Err(e) => {
+                println!("Failed to read from the stream: {}", e);
+            }
         }
     }
 }
